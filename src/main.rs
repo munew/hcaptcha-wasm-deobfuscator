fb220d10c9ee061ac27efac2723b9e6786419e64
Dexv <arazrstmy500+g@gmail.com>
2025-06-15 18:01:08 +0200

    save events
    Added code to parse event data and save it as a json file in asstes\events.json

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,20 @@ mod transformations;
 mod fetcher;
 
 use std::time::Instant;
+use std::collections::HashMap;
 use crate::transformations::Transformer;
 use walrus::Module;
 use crate::fetcher::events::fetch_events;
 use crate::transformations::memory::memory_transformer::MemoryTransformer;
+use std::fs::File;
+use std::io::Write;
+use serde_json;
+
+#[derive(Debug, serde::Serialize)]
+struct Event {
+    id: u32,
+    hash: bool
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let t = Instant::now();
@@ -20,7 +30,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     
     let events = fetch_events(&mut module)?;
-    println!("{:?}", events);
+    let mut events_map: HashMap<String, Event> = HashMap::new();
+    
+    for line in events.lines() {
+        let parts: Vec<&str> = line.split(',').collect();
+        if parts.len() == 3 {
+            let idx = parts[0].to_string();
+            let id = u32::from_str_radix(parts[1], 16)?;
+            let hash = parts[2] == "1";
+            
+            events_map.insert(idx, Event { id, hash });
+        }
+    }
+    
+    let json = serde_json::to_string_pretty(&events_map)?;
+    let mut file = File::create("assets/events.json")?;
+    file.write_all(json.as_bytes())?;
     
     println!("Took {:?}", t.elapsed());
     module.emit_wasm_file("assets/output.wasm")?;
